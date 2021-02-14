e18cdd85246038b4a1d7474c710a8cfde2395f5e
monmon30 <mr.raymondcunanan@gmail.com>
2021-02-14 19:17:49 +0800

    Node basic lessons

---
+++ index.js
@@ -0,0 +1,6 @@
+const Ninja = require('./ninja');
+const path = require('path');
+
+// console.log(global);
+const ninja1 = new Ninja('Uchiha Sasuke', 'Uchiha');
+ninja1.useChidori();
---
+++ ninja.js
@@ -0,0 +1,12 @@
+class Ninja {
+    constructor(name, clan) {
+        this.name = name;
+        this.clan = clan;
+    }
+
+    useChidori() {
+        console.log('Chidoriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!');
+    }
+}
+
+module.exports = Ninja;
