a8980daf7cd6a040377e0a9307d5b99066da499d
monmon30 <mr.raymondcunanan@gmail.com>
2021-02-14 19:41:48 +0800

    NPM Lesson

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
+
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+
+    // lodash
+    // const num = _.random(1, 500);
+    // console.log(num);
+
+    const greet = () => _.once(() => {
+        console.log('hello');
+    })
+    greet();
+
 
     res.setHeader('Content-Type', 'text/html');
 
